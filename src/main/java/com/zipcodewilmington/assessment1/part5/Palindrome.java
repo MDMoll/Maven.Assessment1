7bf1056a60f5216ce2194cfb5f1c421e55646a35
Mark Moll <markmoll@zipcoders-MacBook-Pro-6.local>
2019-02-18 12:45:29 -0500

    2 Palindrome tests passing

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -1,8 +1,30 @@
 package com.zipcodewilmington.assessment1.part5;
 
-public class Palindrome {
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Integer countPalindromes(String input){
-        return null;
+public class Palindrome {
+    int perms = 0;
+
+    public Integer countPalindromes(String input) {
+        int count = 0;
+        ArrayList<String> perms = new ArrayList<>();
+        if (input.length() == 1) {
+            perms.add(input);
+            count++;
+        } else {
+            String chr = input.substring(0, 1);
+            String rest = input.substring(1);
+            ArrayList<String> subPerms = new ArrayList<String>();
+            subPerms.add(rest);
+            for (String s : subPerms) {
+                for (int j = 0; j <= s.length(); j++) {
+                    String newPerm = s.substring(0,j) + chr + s.substring(j);
+                    perms.add(newPerm);
+                }
+            }
+        }
+        return perms.size();
     }
 }
