b981dcca98c138681be12f2d817ff38a2176ba81
Mark Moll <mdmoll@gmail.com>
2019-02-19 13:38:20 -0500

    Post Assessment Update

--- src/main/java/com/zipcodewilmington/assessment1/part1/RockPaperSissorsEvaluator.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/RockPaperSissorsEvaluator.java
@@ -13,7 +13,7 @@ public class RockPaperSissorsEvaluator {
      * @return the respective winning move
      */
     public String getWinningMove(String handSign) {
-        return null;
+        return handSign.equals("rock") ? PAPER : handSign.equals("paper") ? SCISSOR : PAPER;
     }
 
     /**
@@ -21,7 +21,7 @@ public class RockPaperSissorsEvaluator {
      * @return the respective losing move
      */
     public String getLosingMove(String handSign) {
-        return null;
+        return handSign.equals("scissor") ? PAPER : handSign.equals("rock") ? SCISSOR : ROCK;
     }
 
     /**
@@ -30,6 +30,12 @@ public class RockPaperSissorsEvaluator {
      * @return a string representative of the winning hand sign between the two players
      */
     public String getWinner(String handSignOfPlayer1, String handSignOfPlayer2) {
-        return null;
+        if (getLosingMove(handSignOfPlayer1).equals(handSignOfPlayer2)) {
+            return handSignOfPlayer1;
+        } else if (getLosingMove(handSignOfPlayer2).equals(handSignOfPlayer1)) {
+            return handSignOfPlayer2;
+        } else {
+            return "Tie";
+        }
     }
 }
